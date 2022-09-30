bcdffd16b0aae41072dff703a0bb43aec42a7fdf
Cam Song <neosoyn@gmail.com>
2022-09-30 18:31:55 +0800

    update env

--- config/env.js
+++ config/env.js
@@ -96,6 +96,10 @@ function getClientEnvironment(publicUrl) {
       env[key] = JSON.stringify(raw[key]);
       return env;
     }, {}),
+    "__DEV__": true,
+    "__PROFILE__": true,
+    "__UMD__": true,
+    "__EXPERIMENTAL__": true
   };
 
   return { raw, stringified };
