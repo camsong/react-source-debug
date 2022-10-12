46a819d4a8c0bddd35787769d4de8ce125380492
会影 <shaoyin.ssy@alibaba-inc.com>
2022-10-13 05:51:11 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('John Doe');
+
+  useEffect(() => {
+    debugger;
+    console.log('useEffect called');
+  }, [counter]);
+
+  const handleAddClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleMinClick = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>counter: {counter}</p>
+        <div>
+          <button onClick={handleAddClick}>+</button>
+          <button onClick={handleMinClick}>-</button>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
